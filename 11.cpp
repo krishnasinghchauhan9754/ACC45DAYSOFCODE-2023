a03902bc67cccf7bf4bc363f088b844a032ecdf2
krishnasinghchauhan9754 <114243620+krishnasinghchauhan9754@users.noreply.github.com>
2023-09-28 22:53:07 +0530

    Add files via upload

---
+++ 11.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+
+int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+        if(n<=m*36)
+        cout<<"yes"<<endl;
+        else
+        cout<<"no"<<endl;
+        
+    }
+    return 0;
+}
