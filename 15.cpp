8248757fd13d70f8277eee09fb728e95019ad78e
krishnasinghchauhan9754 <114243620+krishnasinghchauhan9754@users.noreply.github.com>
2023-10-03 01:16:25 +0530

    Add files via upload

---
+++ 15.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int n, x;
+        cin>>n>>x;
+        
+        if(x == 0 || n == x)
+        {
+            cout<<0<<endl;
+        }
+        else {
+            if(n-x < x) cout<<(n-x)<<endl;
+            else cout<<x<<endl;
+        }
+    }
+    
+    
+    return 0;
+}
